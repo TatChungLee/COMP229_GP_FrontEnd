c7888716f0fa9c6b02f1ee5aea7161faadba8b6e
Tat Chung Lee <chung5462@gmail.com>
2023-08-01 16:00:32 -0400

    Added Redirects file

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -12,8 +12,13 @@ const AUTH_API = 'https://libbookpro.onrender.com/api/auth/';
 
 const httpOptions = {
 
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-
+  // headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    })
+    
 };
 
  
